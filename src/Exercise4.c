f24f5114bf3abaac5597e92a50190987423ee318
ITITIU20233NgMKhoi <ititiu20233@student.hcmiu.edu.vn>
2021-10-11 19:09:53 +0700

    uooo4

--- src/Exercise4.c
+++ src/Exercise4.c
@@ -8,44 +8,54 @@ Ex:
 |____________________________________|
 */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-int main(int argc, char *argv[]) {
-	//testing variable, applying it to your algorithm for auto-evaluating
-	argc--;
-	int len = argc;
-	int arr[len],i;
-	for(i=0; i<len;i++){
-		arr[i] = atoi(argv[i+1]);
-	}
-	//Your codes here
-	int temp, len;
+int main (int argc, char *argv[])
+{
+    int len = argc;
+    int next, current;
+    char *next_s, *current_s, *temp;	
+    int half = (len - 1) / 2;
 
-    for (i = 0; i < len - 1; i++)
-    {
-        for (int j = i + 1; j < len; j++)
-	    {
-	        if(arr[j] < arr[i])
+	for (int i = 1; i < len; i++)
+	{
+		for(int j = 1; j < half; j++)
+		{
+			current_s = argv[j];
+            next_s = argv[j + 1];
+            current = atoi(current_s);
+            next = atoi(next_s);
+
+	        if(current > next )
 	        {
-	            temp = arr[i];
-	            arr[i] = arr[j];
-	            arr[j] = temp;
+                temp = argv[j];
+	            argv[j] = next_s;
+	            argv[j+1] = temp;
 	        }
-	    }
-    }
-    //printing first half of the array
-  
-    for (i = 0; i < len / 2; i++)
-    {
-        printf ("%d ", arr[i]);
-    }
-    //printing second half of the array
-  
-    for (i = len - 1; i >= len / 2; i--)
-    {
-        printf ("%d ", arr[i]);
-    }
-     return 0;
+		}
+
+		for (int j = half+1; j < len - 1; j++)
+		{
+			current_s = argv[j];
+            next_s = argv[j + 1];
+            current = atoi(current_s);
+            next = atoi(next_s);
+
+	        if(current < next)
+	        {
+                temp = argv[j];
+	            argv[j] = next_s;
+	            argv[j+1] = temp;
+	        }
+		}
+	}
+	// printf("After sorting first half in ascending and second half in descending order:\n");
+	for(int i = 1; i < len; i++)
+	{
+		 printf ("%s ", argv[i]);
+	}
+
+    printf("\n");
+	return 0;
 }
